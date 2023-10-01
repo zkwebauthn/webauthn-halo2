4e17f244b8a1e65845243d6d494a3aab282964cb
Albert Su <albertsu@Alberts-MacBook-Pro.local>
2023-10-01 15:17:15 -0700

    ✨Add challenge to the verifier

--- contracts/src/verifier/Verifier.sol
+++ contracts/src/verifier/Verifier.sol
@@ -3,23 +3,41 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
+/**
+ * Each account has a public key stored on chain.
+ * This contract is used to verify the signature of the passkey for an account via a zk proof.
+ * The zk proof states that the account's public key signed a specific piece of challenge data.
+ * 
+ * @title Verifier
+ * @author Know Nothing Labs
+ */
 contract Verifier is Ownable2Step {
-    mapping(address => bytes) public challenges;
+    mapping(address => bytes) public publicKeys;
     address public snarkVerifier;
+
+    event Verification(address indexed account, bool success);
 
     constructor(address _snarkVerifier){
         snarkVerifier = _snarkVerifier;
     }
 
-	// Stores the public key for an account
-	function registerNewPasskey(address account, bytes memory expectedChallenge) external onlyOwner {
-        challenges[account] = expectedChallenge;
+    /**
+     *
+     * @param account Address of the account to register the public key for
+     * @param publicKey Public Key of he account
+     */
+	function registerNewPasskey(address account, bytes calldata publicKey) external onlyOwner{
+        publicKeys[account] = publicKey;
     }
 
-	// Makes a call to Verifier checking the proof vs the account's public key
-	// returns true if proof matches public key
-	function verifyPasskeySignature(address account, bytes memory proof) external returns(bool){
-        (bool success,) = snarkVerifier.call(abi.encode(challenges[account], proof));
+    /**
+     * @dev Verifies a passkey signature for an account - return true if the verification is successful
+     * @param account Address of the account to verify the passkey signature for
+     * @param proof Zk proof of the passkey signature
+     */
+	function verifyPasskeySignature(address account, bytes memory proof, bytes memory challenge) external returns(bool) {
+        (bool success,) = snarkVerifier.call(abi.encode(publicKeys[account], proof));
+        emit Verification(account, success);
         return success;
     }
 }
